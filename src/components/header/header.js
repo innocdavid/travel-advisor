fdd83352c82802d78f1da15d3d4da76e780737e8
Banja David Lotigo <banja@kali.lan>
2023-02-02 08:22:09 -0500

    PAGE HEADER - Add application name - Add search inpout box with text - Style page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Box, Typography } from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search';
+
+import { Search, SearchIconWrapper, StyledInputBase, AppToolbar } from './style';
 
 const Header = () => {
-    return (
-        <h1>Header</h1>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <AppToolbar>
+          <Typography
+            variant="h5"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Travel Advisor
+          </Typography>
+          <Box display="flex">
+            <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+                Explore new places
+            </Typography>
+            {/* <Autocomplete> */}
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                    placeholder="Search…"
+                    inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
+            {/* </Autocomplete> */}
+            
+          </Box>
+          
+        </AppToolbar>
+      </AppBar>
+    </Box>
     );
 }
 
